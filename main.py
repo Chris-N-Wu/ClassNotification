218c4582ab1ca0bd911dd30ba9acf97810d1deda
Chris-N-Wu <dolphins0248@gmail.com>
2023-08-09 15:42:29 -0400

    Can now loop through courses in stored Data

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ class DataJson:
 
         self.json_file_path = json_file_path
         self.term_code = json_data['termCode']
+        self.course_ids = json_data['ids']
         self.courses = json_data['data']
 
     """
@@ -36,7 +37,8 @@ class DataJson:
     """
 
     # adding a course to be tracked
-    def add_tracked_course(self, course_id, course_name, course_crn, seats_available, wait_available):
+    def add_tracked_course(self, course_id, course_name, course_crn, seats_available, wait_available, subject,
+                           course_number):
         # checking that this course hasn't already been added to be tracked
         for course in self.courses:
             if course_id == course['courseId']:
@@ -47,7 +49,9 @@ class DataJson:
                              'courseName': course_name,
                              'courseCrn': course_crn,
                              'seatsAvailable': seats_available,
-                             'waitAvailable': wait_available})
+                             'waitAvailable': wait_available,
+                             'lookupInfo': {'subject': subject,
+                                            'courseNumber': course_number}})
 
     def collect_ids(self):
         ids = []
@@ -84,22 +88,22 @@ class BannerDataJson:
     def get_course_name(self, course_id):
         for course in self.courses:
             if course['id'] == course_id:
-                return course['id']['courseTitle']
+                return course['courseTitle']
 
     def get_seats_available(self, course_id):
         for course in self.courses:
             if course['id'] == course_id:
-                return course['id']['seatsAvailable']
+                return course['seatsAvailable']
 
     def get_waitlist_seats(self, course_id):
         for course in self.courses:
             if course['id'] == course_id:
-                return course['id']['waitCapacity']
+                return course['waitAvailable']
 
 
 # setting the discord webhook
 webhook = SyncWebhook.from_url(url=config.webhook_url)
-
+"""
 # gaining access to our session
 clickContinue = requests.post(
     url="https://nubanner.neu.edu/StudentRegistrationSsb/ssb/term/search?mode=search",
@@ -139,23 +143,68 @@ print(get.text)
 
 response_json = get.json()
 
-banner_stuff = DataJson('data.json')
-banner_stuff.add_tracked_course(course_id=123,
-                                course_name="abc",
-                                course_crn=123,
-                                seats_available=5,
-                                wait_available=0)
-banner_stuff.save_json()
 
 seats_remaining = response_json['data'][1]['seatsAvailable']
 
 # user id for Dolphins0248
 # webhook.send('<@350046129892622336> IT WORKS!!!')
 # webhook.send("Database Design Has " + str(seats_remaining) + " Seats Remaining")
+"""
 
 if __name__ == '__main__':
-    a = sys.argv[1]
-    print(a)
+    # term code for Fall 2023-24 school year
+    term_code = '202410'
+
+    # gaining access to our session
+    clickContinue = requests.post(
+        url="https://nubanner.neu.edu/StudentRegistrationSsb/ssb/term/search?mode=search",
+        data={
+            "term": "202410"
+        },
+    )
+
+    # parsing the returned session
+    headers = clickContinue.headers['Set-Cookie']
+    headers = headers.split(';')
+    # retrieving the JSESSIONID
+    jsessionid = headers[0].split('=')[1]
+    # retrieving nu banner-cookie
+    nu_banner_cookie = headers[3].split('=')[1]
+
+    # setting cookies
+    cookies = {"nubanner-cookie": nu_banner_cookie, "JSESSIONID": jsessionid}
 
     # loading saved data
     saved_data = DataJson('data.json')  # TODO: Replace hard coded path
+
+    # looping through each course in the saved courses to check their enrollment numbers
+    for course in saved_data.courses:
+        subject = course['lookupInfo']['subject']
+        course_number = course['lookupInfo']['courseNumber']
+
+        # string for course lookup
+        search_url = 'https://nubanner.neu.edu/StudentRegistrationSsb/ssb/searchResults/searchResults?' \
+                     f'txt_subject={subject}' \
+                     f'&txt_courseNumber={course_number}' \
+                     f'&txt_term={term_code}' \
+                     '&startDatepicker=' \
+                     '&endDatepicker=&pageOffset=0' \
+                     '&pageMaxSize=100' \
+                     '&sortColumn=subjectDescription&sortDirection=asc'
+
+        print(search_url)
+
+        get = requests.get(url=search_url,
+                           cookies=cookies)
+
+        # print(get.json())
+        banner_data = BannerDataJson(get.json())
+        print(course['courseName'])
+        print(banner_data.get_seats_available(course['courseId']))
+
+        # resets input from last search
+        post = requests.post(
+            url='https://nubanner.neu.edu/StudentRegistrationSsb/ssb/classSearch/resetDataForm',
+            cookies=cookies
+        )
+
