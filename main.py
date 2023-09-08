df233ace431bcfa147f7af1c50af1f4dbceb9a2e
Chris-N-Wu <dolphins0248@gmail.com>
2023-09-08 18:48:06 -0400

    Now able to add courses simply by specifying subject, course number, and course crn
    Application will now loop through tracked courses every 15 sections and only ping user when a seat is open and when changes from stored seat count

--- main.py
+++ main.py
@@ -28,55 +28,46 @@ class DataJson:
 
         self.json_file_path = json_file_path
         self.term_code = json_data['termCode']
-        self.course_ids = json_data['ids']
         self.courses = json_data['data']
 
     """
     Getters
     """
 
-    def get_seats_available(self, course_id):
+    def get_seats_available(self, course_crn):
         for course in self.courses:
-            if course_id == course['courseId']:
+            if course_crn == course['courseCrn']:
                 return course['seatsAvailable']
 
     # updating the number of seats available for this course
-    def update_course_seats(self, course_id, new_seats):
+    def update_course_seats(self, course_crn, new_seats):
         # something cool, 'course' is a reference to the particular item in the list,
         # so when updating 'course', the item itself in the list is updated as well
         for course in self.courses:
-            if course_id == course['courseId']:
+            if course_crn == course['courseCrn']:
                 course['seatsAvailable'] = new_seats
 
     # adding a course to be tracked
-    def add_tracked_course(self, course_id, course_name, course_crn, seats_available, wait_available, subject,
+    def add_tracked_course(self, course_name, course_crn, seats_available, wait_available, subject,
                            course_number):
         # checking that this course hasn't already been added to be tracked
         for course in self.courses:
-            if course_id == course['courseId']:
+            if course_crn == course['courseCrn']:
+                print("Error: Course Already Exists In System!")
                 return
 
         # adding the course to be tracked
-        self.courses.append({'courseId': course_id,
-                             'courseName': course_name,
+        self.courses.append({'courseName': course_name,
                              'courseCrn': course_crn,
                              'seatsAvailable': seats_available,
                              'waitAvailable': wait_available,
                              'lookupInfo': {'subject': subject,
                                             'courseNumber': course_number}})
 
-    def collect_ids(self):
-        ids = []
-        for course in self.courses:
-            ids.append(course['courseId'])
-
-        return ids
-
     # saving the current information
     def save_json(self):
         # serializing the json
         json_object = json.dumps({'termCode': self.term_code,
-                                  'ids': self.collect_ids(),
                                   'data': self.courses},
                                  indent=2)
 
@@ -89,33 +80,59 @@ class DataJson:
 class BannerDataJson:
     """Breaks down returned Banner JSONs into manageable dictionary"""
 
-    def __init__(self, json_input):
-        self.json_input = json_input
-        self.courses = self.json_input['data']
+    def __init__(self, json_output):
+        self.json_input = json_output
+        self.courses = json_output['data']
+
+    def __getitem__(self, item):
+        return self.json_input[item]
+
+    def validate_crn(self, course_crn):
+        course_crn_list = []
+
+        for course in self.courses:
+            course_crn_list.append(course['courseReferenceNumber'])
+
+        if str(course_crn) not in course_crn_list:
+            return 0
+        else:
+            return 1
 
     """
     Getters
     """
 
-    def get_course_name(self, course_id):
+    def get_course_name(self, course_crn):
         for course in self.courses:
-            if course['id'] == course_id:
+            if course['courseReferenceNumber'] == str(course_crn):
                 return course['courseTitle']
 
-    def get_seats_available(self, course_id):
+    def get_seats_available(self, course_crn):
         for course in self.courses:
-            if course['id'] == course_id:
+            if course['courseReferenceNumber'] == str(course_crn):
                 return course['seatsAvailable']
 
-    def get_waitlist_seats(self, course_id):
+    def get_waitlist_seats(self, course_crn):
         for course in self.courses:
-            if course['id'] == course_id:
+            if course['courseReferenceNumber'] == str(course_crn):
                 return course['waitAvailable']
 
 
 # user id for Dolphins0248
 # webhook.send('<@350046129892622336> IT WORKS!!!')
 # webhook.send("Database Design Has " + str(seats_remaining) + " Seats Remaining")
+
+
+# string for course_data lookup
+def search_url(subject, course_number):
+    return 'https://nubanner.neu.edu/StudentRegistrationSsb/ssb/searchResults/searchResults?' \
+           f'txt_subject={subject}' \
+           f'&txt_courseNumber={course_number}' \
+           f'&txt_term={config.term_code}' \
+           '&startDatepicker=' \
+           '&endDatepicker=&pageOffset=0' \
+           '&pageMaxSize=100' \
+           '&sortColumn=subjectDescription&sortDirection=asc'
 
 
 class Application:
@@ -127,7 +144,7 @@ class Application:
 
     def start_app(self):
         # gaining access to our session
-        click_continue = requests.post(
+        start_session = requests.post(
             url='https://nubanner.neu.edu/StudentRegistrationSsb/ssb/term/search?mode=search',
             data={
                 'term': config.term_code
@@ -135,7 +152,7 @@ class Application:
         )
 
         # parsing the returned session
-        headers = click_continue.headers['Set-Cookie']
+        headers = start_session.headers['Set-Cookie']
         headers = headers.split(';')
         # retrieving the JSESSIONID
         jsessionid = headers[0].split('=')[1]
@@ -156,32 +173,56 @@ class Application:
             subject = course_data['lookupInfo']['subject']
             course_number = course_data['lookupInfo']['courseNumber']
 
-            # string for course_data lookup
-            search_url = 'https://nubanner.neu.edu/StudentRegistrationSsb/ssb/searchResults/searchResults?' \
-                         f'txt_subject={subject}' \
-                         f'&txt_courseNumber={course_number}' \
-                         f'&txt_term={config.term_code}' \
-                         '&startDatepicker=' \
-                         '&endDatepicker=&pageOffset=0' \
-                         '&pageMaxSize=100' \
-                         '&sortColumn=subjectDescription&sortDirection=asc'
-
-            get = requests.get(url=search_url,
+            get = requests.get(url=search_url(subject, course_number),
                                cookies=self.cookies)
 
-            # print(get.json())
+            print(get.json())
             banner_data = BannerDataJson(get.json())
             print(course_data['courseName'])
-            print(banner_data.get_seats_available(course_data['courseId']))
+            print(banner_data.get_seats_available(course_data['courseCrn']))
 
-            if banner_data.get_seats_available(course_data['courseId']) != \
-                    self.saved_data.get_seats_available(course_data['courseId']):
-                print("A")
+            # if banner_data.get_seats_available(course_data['courseCrn']) != \
+            #         self.saved_data.get_seats_available(course_data['courseCrn']):
+            #     print("A")
+
+            banner_seats_available = banner_data.get_seats_available(course_data['courseCrn'])
+            if (banner_seats_available > 0
+                    and banner_seats_available != self.saved_data.get_seats_available(course_data['courseCrn'])):
+                data = {'content': f'{config.discord_user_id} A Seat Is Open in '
+                                   + course_data['courseName']
+                                   + ", CRN: "
+                                   + str(course_data['courseCrn'])}
+                requests.post(url=config.webhook_url,
+                              data=data)
 
             self.reset_inputs()
 
-            data = {'content': "HI!"}
-            requests.post(url=config.webhook_url, data=data)
+    # Allows user to add new course to tracked courses
+    def add_course(self, subject, course_number, course_crn):
+        # Finding the course
+        get = requests.get(url=search_url(subject, course_number),
+                           cookies=self.cookies)
+
+        # converting to custom parsable class
+        banner_data = BannerDataJson(get.json())
+
+        print(get.text)
+
+        if banner_data.validate_crn(course_crn) == 0:
+            print("INVALID SEARCH")
+            return
+
+        if not (banner_data['totalCount'] == 0):
+            self.saved_data.add_tracked_course(course_name=banner_data.get_course_name(course_crn),
+                                               course_crn=course_crn,
+                                               seats_available=banner_data.get_seats_available(course_crn),
+                                               wait_available=banner_data.get_waitlist_seats(course_crn),
+                                               subject=subject,
+                                               course_number=course_number)
+
+            self.saved_data.save_json()
+        else:
+            print("INVALID FIELD PROVIDED")
 
     def reset_inputs(self):
         # resets input from last search
@@ -192,8 +233,9 @@ class Application:
 
 
 if __name__ == '__main__':
+    myApplication = Application('dataActual.json')
 
-    myApplication = Application('data.json')
+    # myApplication.add_course(subject="MATH", course_number=2341, course_crn=12074)
 
     while True:
         myApplication.lookup_courses()
